9888fa6490da37d39c9104cc3db8e8500ce9540f
Osmar Bruno <brunoosmar442@gmail.com>
2022-09-17 16:11:40 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import {promise as fs} from 'fs';
+
+async function getDataJson()
+{
+  try
+  {
+    const json = JSON.parse(await fs.readFile('./src/estados-cidades-atividade.json'));
+    return json;
+  }catch (error)
+  {
+    console.log('Ta dando erro oh: ', error);
+  }
+}
